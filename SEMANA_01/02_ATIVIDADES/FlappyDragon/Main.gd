45ffe825ad0db4712dc8fec7f6a4996cdebf85b7
uMorita <pedromorita19@gmail.com>
2023-02-04 18:51:44 -0300

    Programação Flappy Dragon

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
-var status = 1
-var vscore = 0
-var x = 1.5 
-var y = 1.5 
+var status = 1 #define se o dragão está voando
+var vscore = 0 #define o score do jogo 
+var x = 1.5 #define a posição horizontal
+var y = 1.5 #defina a posição vertical 
 
 # executa essa função ao carregar o jogo
 func _ready():
@@ -17,12 +17,12 @@ func _process(delta):
 	if status == 1: # jogando
 		
 		# movimenta o cenário do fundo
-		$background.position.x -= 1*x
+		$background.position.x -= 3*x
 		if ($background.position.x) < -200:
 			$background.position.x = 600
 			
 		# movimenta as colunas para colisão
-		$columns.position.x -= 2*x
+		$columns.position.x -= 3*x
 		if ($columns.position.x) < -550:
 			$columns.position.x = rand_range(0, 350) - 50
 			$columns.position.y = rand_range(0, 400) - 200
@@ -41,11 +41,11 @@ func _process(delta):
 			
 		# se apertou seta para baixo, aumenta o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_down"):
-			$dragon.position.y += 2
+			$dragon.position.y += 8
 
 		# se apertou seta para cima, diminui o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_up"):
-			$dragon.position.y -= 4
+			$dragon.position.y -= 8
 			
 	elif status == 0: # parado
 		
